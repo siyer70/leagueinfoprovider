b78b7385a9497df1df9cb9d944321d95793bd3da
chasrini <shekhar.shekhar@publicissapient.com>
2019-08-10 23:48:36 +0530

    nodejs component build added

--- build.sh
+++ build.sh
@@ -10,6 +10,18 @@ if [ $ret -eq 0 ]; then
 	cp infoserver/target/*.jar release/
 	tar -czvf release.tar.gz release/
 	echo "API server build succeeded."
+	echo "Building UI Component.."
+	cd league-info-app/
+	echo "Building UI Component.."
+	npm run build
+	ret=$?
+	cd ..
+	if [ $ret -eq 0 ]; then
+		cp -R league-info-app/build release/
+		echo "UI Component built successfully."
+	else
+		echo "UI Component build failed, terminating.."
+	fi	
 else
 	echo "API server build failed, terminating.."
 fi
