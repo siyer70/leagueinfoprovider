6c0e5b95ae637ecd2e702419237e2c9b9bd2e939
chasrini <shekhar.shekhar@publicissapient.com>
2019-08-10 19:27:15 +0530

    Jenkins script file

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Building API Server.."
+currdir=`pwd`
+mkdir -p release
+cd infoserver/
+mvn clean install
+ret=$?
+cd ..
+if [ $ret -eq 0 ]; then
+	cp infoserver/target/*.jar release/
+	echo "API server build succeeded."
+	cd league-info-app/
+	echo "Building UI Component.."
+	npm run build
+	ret=$?
+	cd ..
+	if [ $ret -eq 0 ]; then
+		cp -R league-info-app/build release/
+		echo "UI Component built successfully."
+	fi
+else
+	echo "API server build failed, terminating.."
+fi
+exit $ret
