d7397836327642c0db514d142bfae15c85cd8e2c
chasrini <shekhar.shekhar@publicissapient.com>
2019-08-18 20:38:32 +0530

    Fixes for bg processes

--- jenkins/scripts/start-selenium-server.sh
+++ jenkins/scripts/start-selenium-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+PLATFORM=$1
 echo "Starting Selenium Server.."
 
-java -jar /tmp/selenium-server/selenium-server-standalone-2.48.2.jar -browser "browserName=chrome, version=76.0, platform=MAC" -Dwebdriver.chrome.driver=/tmp/selenium-server/MAC/chromedriver > /tmp/selenium-server.log 2>&1 &
+java -jar /tmp/selenium-server/selenium-server-standalone-2.48.2.jar -browser "browserName=chrome, version=76.0" -Dwebdriver.chrome.driver=/tmp/selenium-server/$PLATFORM/chromedriver > /tmp/selenium-server.log 2>&1 &
 echo "waiting for Selenium Server to come up"
 sleep 3
 SELENIUM_SERVER_PID=`ps -ef | grep -v -e 'grep ' | grep -i "java " | grep -i "selenium-server-standalone" | tr -s " " | cut -d " " -f 2`
