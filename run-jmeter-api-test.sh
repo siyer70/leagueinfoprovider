b9d1f272dcc8af0639d8ad652e3f44dcec8ab091
chasrini <shekhar.shekhar@publicissapient.com>
2019-08-30 14:29:11 +0530

    jmeter script file updated

--- run-jmeter-api-test.sh
+++ run-jmeter-api-test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/Users/chasrini/apache-jmeter-5.1.1/bin/jmeter -n -t jmeter-tests/InfoserverAPITest.jmx -l jmeter-tests/logs/jmeter-text-log.jtl -j jmeter-tests/logs/jmeter-run.log
+mkdir -p jmeter-tests/logs
+jmeter -n -t jmeter-tests/InfoserverAPITest.jmx -l jmeter-tests/logs/jmeter-text-log.jtl -j jmeter-tests/logs/jmeter-run.log
