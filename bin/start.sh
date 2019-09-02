c7d52621a36a40296daa13679b2138c58e43249b
chasrini <shekhar.shekhar@publicissapient.com>
2019-09-02 17:27:14 +0530

    start scripts updated for dev env

--- bin/start.sh
+++ bin/start.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 echo "Starting API Server.."
-VERSION=${1:-"1.0"}
+ENVIRONMENT=${1:-"PROD"}
+VERSION=${2:-"1.0"}
+ENVIRONMENT=`echo $ENVIRONMENT | tr a-z A-Z`
 mkdir -p logs
-nohup java -Dserver.port=8500 -jar releases/PROD/infoserver-$VERSION/infoserver-$VERSION.jar > logs/apiserver.log 2>&1 &
+nohup java -Dserver.port=8500 -jar releases/$ENVIRONMENT/infoserver-$VERSION/infoserver-$VERSION.jar > logs/apiserver.log 2>&1 &
 echo "waiting for API Server to come up"
 sleep 10
+API_SERVER_PID=`ps -ef | grep -v -e 'grep ' | grep -i "java " | grep -i "$ENVIRONMENT" | grep -i "infoserver-$VERSION.jar" | tr -s " " | cut -d " " -f 2`
+if [ -z "$API_SERVER_PID" ]
+then
+      echo "API Server not running"
+else
+        echo "API Server running with PID: $API_SERVER_PID"
+fi
+
 echo "Starting Web Server.."
-nohup serve -s releases/PROD/infoserver-$VERSION/build -l 8400 > logs/ui.log 2>&1 &
-sleep 5
-echo "Happy browsing!"
+if [ "$ENVIRONMENT" == "PROD" ]; then
+	nohup serve -s releases/$ENVIRONMENT/infoserver-$VERSION/build -l 8400 > logs/ui.log 2>&1 &
+	sleep 5
+	WEB_SERVER_PID=`ps -ef | grep -v -e 'grep ' | grep -i "serve" | grep -i "8400"| tr -s " " | cut -d " " -f 2`
+	if [ -z "$WEB_SERVER_PID" ]
+	then
+	      echo "Web Server not running"
+	else
+	      echo "Web Server running with PID: $WEB_SERVER_PID"
+	      echo "Happy browsing!"
+	fi	
+fi
+
+if [ "$ENVIRONMENT" == "DEV" ]; then
+	nohup sh -c 'cd league-info-app/ && npm start' > logs/ui.log 2>&1 &
+	sleep 2
+	WEB_SERVER_PID=`ps -ef | grep -v -e 'grep ' | grep -i "react-scripts" | grep -i "start.js"| tr -s " " | cut -d " " -f 2`
+	if [ -z "$WEB_SERVER_PID" ]
+	then
+	      echo "Web Server not running"
+	else
+	      echo "Web Server running with PID: $WEB_SERVER_PID"
+	      echo "Happy browsing!"
+	fi	
+fi
--- bin/status.sh
+++ bin/status.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
-VERSION=${1:-"1.0"}
-API_SERVER_PID=`ps -ef | grep -v -e 'grep ' | grep -i "java " | grep -i "infoserver-$VERSION.jar" | tr -s " " | cut -d " " -f 2`
+ENVIRONMENT=${1:-"PROD"}
+VERSION=${2:-"1.0"}
+ENVIRONMENT=`echo $ENVIRONMENT | tr a-z A-Z`
+API_SERVER_PID=`ps -ef | grep -v -e 'grep ' | grep -i "java " | grep -i "$ENVIRONMENT" | grep -i "infoserver-$VERSION.jar" | tr -s " " | cut -d " " -f 2`
 if [ -z "$API_SERVER_PID" ]
 then
       echo "API Server not running"
 else
         echo "API Server running with PID: $API_SERVER_PID"
 fi
-WEB_SERVER_PID=`ps -ef | grep -v -e 'grep ' | grep -i "serve" | grep -i "8400"| tr -s " " | cut -d " " -f 2`
-if [ -z "$WEB_SERVER_PID" ]
-then
-      echo "Web Server not running"
-else
-        echo "Web Server running with PID: $WEB_SERVER_PID"
+
+if [ "$ENVIRONMENT" == "PROD" ]; then
+	WEB_SERVER_PID=`ps -ef | grep -v -e 'grep ' | grep -i "serve" | grep -i "8400"| tr -s " " | cut -d " " -f 2`
+	if [ -z "$WEB_SERVER_PID" ]
+	then
+	      echo "Web Server not running"
+	else
+	        echo "Web Server running with PID: $WEB_SERVER_PID"
+	fi
 fi
 
+if [ "$ENVIRONMENT" == "DEV" ]; then
+	WEB_SERVER_PID=`ps -ef | grep -v -e 'grep ' | grep -i "react-scripts" | grep -i "start.js"| tr -s " " | cut -d " " -f 2`
+	if [ -z "$WEB_SERVER_PID" ]
+	then
+	      echo "Web Server not running"
+	else
+	        echo "Web Server running with PID: $WEB_SERVER_PID"
+	fi
+fi
