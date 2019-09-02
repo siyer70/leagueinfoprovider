03a346906feac1c259f5b188c928eecae20ca624
chasrini <shekhar.shekhar@publicissapient.com>
2019-09-02 09:35:57 +0530

    logs dir created explicitly before starting the process in start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Starting API Server.."
 VERSION=${1:-"1.0"}
+mkdir -p logs
 nohup java -Dserver.port=8500 -jar releases/PROD/infoserver-$VERSION/infoserver-$VERSION.jar > logs/apiserver.log 2>&1 &
 echo "waiting for API Server to come up"
 sleep 10
