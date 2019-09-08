5710070c27b98c342e54c9874c56639a365424a4
chasrini <shekhar.shekhar@publicissapient.com>
2019-09-08 17:12:03 +0530

    Duplicate code fix in the model

--- infoserver/src/main/java/com/fbleague/infoserver/model/Position.java
+++ infoserver/src/main/java/com/fbleague/infoserver/model/Position.java
@@ -4,19 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Position {
-
-	@JsonProperty("country_id")
-	private String countryId;
-
-	@JsonProperty("country_name")
-	private String countryName;
-
-	@JsonProperty("league_id")
-	private String leagueId;
-
-	@JsonProperty("league_name")
-	private String leagueName;
+public class Position extends League {
 
 	@JsonProperty("team_id")
 	private String teamId;
@@ -26,38 +14,6 @@ public class Position {
 
 	@JsonProperty("overall_league_position")
 	private String overallLeaguePosition;
-
-	public String getCountryId() {
-		return countryId;
-	}
-
-	public void setCountryId(String countryId) {
-		this.countryId = countryId;
-	}
-
-	public String getCountryName() {
-		return countryName;
-	}
-
-	public void setCountryName(String countryName) {
-		this.countryName = countryName;
-	}
-
-	public String getLeagueId() {
-		return leagueId;
-	}
-
-	public void setLeagueId(String leagueId) {
-		this.leagueId = leagueId;
-	}
-
-	public String getLeagueName() {
-		return leagueName;
-	}
-
-	public void setLeagueName(String leagueName) {
-		this.leagueName = leagueName;
-	}
 
 	public String getTeamId() {
 		return teamId;
